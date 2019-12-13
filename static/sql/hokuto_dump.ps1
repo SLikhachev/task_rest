45d93a846491577585027914f95796d57df64fa3
aughingl <aughingl@gmail.com>
2019-12-13 14:57:39 +1000

    text sql folder modified

--- static/sql/hokuto_dump.ps1
+++ static/sql/hokuto_dump.ps1
@@ -1,7 +1,8 @@
 $date = Get-Date -F "%d-MM-%y"
-$BACKUP_DIR = "E:\pgsql\dump\"
+$BACKUP_DIR = "C:\Users\Public\db_backup\pg_dump\"
 $BACKUP_FILE = $BACKUP_DIR + "hokuto_" + $date + ".backup"
 #echo $BACKUP_FILE
  $env:PGPASSWORD="boruh"
  "pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f " + $BACKUP_FILE  + " hokuto" | iex
- 
+  $stale= (Get-Date).AddDays(-7)
+  ls $BACKUP_DIR | ? {$_.Creationtime -lt $stale; } | del -Force
