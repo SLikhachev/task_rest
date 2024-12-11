085ced11399db8992ecb5272107c1119a1d0342e
SLikhachev <aughingl@gmail.com>
2024-12-11 18:42:08 +1000

    HOW TO file added

--- poly/clinic/__init__.py
+++ poly/clinic/__init__.py
@@ -4,6 +4,6 @@ from flask_restful import Api
 bp = Blueprint('clinic', __name__, url_prefix='/clinic/')
 api = Api(bp)
 
-from poly.clinic import views
+from poly.clinic import views # noqa: E402
 api.add_resource(views.Hello, 'hello')
 #import poly.clinic.views
--- poly/sprav/__init__.py
+++ poly/sprav/__init__.py
@@ -6,5 +6,5 @@ bp = Blueprint('sprav', __name__, url_prefix='/sprav')
 api = Api(bp)
 
 # correct pmu tarifs
-from poly.sprav.tarif.pmu import task as tarif_pmu
+from poly.sprav.tarif.pmu import task as tarif_pmu # noqa: E402
 api.add_resource(tarif_pmu.UpsertTarifs, '/tarif/pmu', endpoint='tarif_pmu')
